cc61d482d8e3bfbad7a9193e785c414563fb7b00
b02902131 <b02902131@ntu.edu.tw>
2018-04-19 14:54:03 +0800

    Check Arguments

--- coreml_to_onnx.py
+++ coreml_to_onnx.py
@@ -1,11 +1,26 @@
 import sys
 
+##################
+## Check package
+##################
+
+
+##################
+## Check arguments
+##################
+
 if len(sys.argv) < 3:
-    print("Usage: python convertCoreML.py [coreML model name] [onnx model name]")
+    print("\t (!) Usage: python convertCoreML.py [coreML model name] [onnx model name]")
+    print("\t\t * coreML_model : The name of the model you want to convert, ended with \".mlmodel\".")
+    print("\t\t * onnx_model \t: The output name of the onnx model.")
     exit()
 else:
     coreML_name = sys.argv[1]
     onnx_name = sys.argv[2]
+
+if len(coreML_name) <= len(".mlmodel") or coreML_name[-len(".mlmodel"):] != ".mlmodel":
+    print("\t (!) File name error: ", coreML_name, "is not a coreMLmodel. It's not ended with \".mlmodel\"")
+    exit()
 
 from coremltools.models.utils import load_spec
 model_coreml = load_spec(coreML_name)
